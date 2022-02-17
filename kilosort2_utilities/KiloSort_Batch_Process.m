ec8c43194e85fda2e0e99355fe9df19526667755
jms7008 <99907908+jms7008@users.noreply.github.com>
2022-02-17 13:49:11 -0500

    Update KiloSort_Batch_Process.m

--- kilosort2_utilities/KiloSort_Batch_Process.m
+++ kilosort2_utilities/KiloSort_Batch_Process.m
@@ -1,13 +1,16 @@
 %Create a binary file and run kilosort on all the files
+%All of the data is saved in .mat files for each animal and day of recording
 
-file_dir='C:\Users\Admin\Dropbox (ListonLab)\DataForJoe_SOMcreBLArec\';
-data_dir='DFC Protocol\';
+file_dir='C:\Users\Admin\XXXXXX\';
+data_dir='XXXXXXXX\';
 animal_dirs=dir([file_dir data_dir]);
 animal_dirs=animal_dirs(3:end);
-day_headers={'HAB','PreHabLaser','PreRecallLaser','RECALL','TRAINING'};
-exclude_electrodes=[32];
+
+%specify what to look for in your file names if there are different days:
+day_headers={'RECALL'};
+exclude_electrodes=[];%specify if there are rows in your data to exclude
 for animal_rep=1:length(animal_dirs)
-    for day_rep=4
+    for day_rep=1:length(day_headers)
         clear mat_file
         day_dir=dir([file_dir data_dir animal_dirs(animal_rep).name,'\','*',day_headers{day_rep}]); 
         day_dir=[day_dir.folder,'\',day_dir.name];
